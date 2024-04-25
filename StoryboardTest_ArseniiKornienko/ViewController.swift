6bea734836262619bea88dad52883fb0bd129ff1
Arseny89 <167592505+Arseny89@users.noreply.github.com>
2024-04-25 01:24:28 -0700

    Initial commit

---
+++ StoryboardTest_ArseniiKornienko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryboardTest_ArseniiKornienko
+//
+//  Created by Арсений Корниенко on 4/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
